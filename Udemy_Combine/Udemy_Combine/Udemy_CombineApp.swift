0143f7f885f8efafb6d025b8696ec16daec02499
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-06 11:03:12 -0500

    add Udemy_Combine class project

---
+++ Udemy_Combine/Udemy_Combine/Udemy_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udemy_CombineApp.swift
+//  Udemy_Combine
+//
+//  Created by Thomas Cowern on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Udemy_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

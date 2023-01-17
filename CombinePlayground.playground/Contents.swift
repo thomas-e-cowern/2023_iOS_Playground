46139568c0b13a768182b885b9f1610ae3ba3d69
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-17 07:34:25 -0500

    learn map and formatter with publisher and sink

--- CombinePlayground.playground/Contents.swift
+++ CombinePlayground.playground/Contents.swift
@@ -1,6 +1,16 @@
 import UIKit
 import Combine
 
+// Collect
 ["A", "B", "C", "D", "E"].publisher.collect(3).sink {
+    print($0)
+}
+
+let formatter = NumberFormatter()
+formatter.numberStyle = .spellOut
+
+[123, 45, 67, 1234].publisher.map {
+    formatter.string(from: NSNumber(integerLiteral: $0)) ?? ""
+}.sink {
     print($0)
 }

b46053cfa0ecd1e364bbf8cdc62a98175ecbc28f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-26 07:38:32 -0500

    learn removeDuplicates

--- CombinePlayground.playground/Contents.swift
+++ CombinePlayground.playground/Contents.swift
@@ -5,3 +5,12 @@ let numbers = (1...20).publisher
 
 numbers.filter { $0 % 3 == 0 }.sink { print($0) }
 
+let words = "apple apple fruit apple apple mango watermelon apple".components(separatedBy: " ").publisher
+
+words.sink {
+    print($0)
+}
+
+words.removeDuplicates().sink {
+    print("Duplicates removed:", $0)
+}

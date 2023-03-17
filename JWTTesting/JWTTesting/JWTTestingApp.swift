06d3f1059338ac74bded87d8313a76fcdd468105
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-03-17 15:37:39 -0400

    add JWTTesting

---
+++ JWTTesting/JWTTesting/JWTTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JWTTestingApp.swift
+//  JWTTesting
+//
+//  Created by Thomas Cowern on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct JWTTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

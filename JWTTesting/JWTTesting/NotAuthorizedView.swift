fe34d56020378f354a55328594bd0d369b87f2e2
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-03-21 14:17:35 -0400

    add NotAuthorizedView and update ContentVie

---
+++ JWTTesting/JWTTesting/NotAuthorizedView.swift
@@ -0,0 +1,20 @@
+//
+//  NotAuthorizedView.swift
+//  JWTTesting
+//
+//  Created by Thomas Cowern on 3/21/23.
+//
+
+import SwiftUI
+
+struct NotAuthorizedView: View {
+    var body: some View {
+        Text("You are not authorized")
+    }
+}
+
+struct NotAuthorizedView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotAuthorizedView()
+    }
+}

b95f7874f3b7dd6747cac88225aa179b72e6f076
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-03-22 08:36:55 -0400

    NavigationStackAPI-2 initial commit

---
+++ NavigationStackAPI-2/NavigationStackAPI-2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NavigationStackAPI-2
+//
+//  Created by Thomas Cowern on 3/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationStackAPI-2/NavigationStackAPI-2/NavigationStackAPI_2App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackAPI_2App.swift
+//  NavigationStackAPI-2
+//
+//  Created by Thomas Cowern on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackAPI_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

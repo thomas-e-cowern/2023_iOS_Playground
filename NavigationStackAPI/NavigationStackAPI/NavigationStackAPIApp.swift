c898fa4c32d26d09c7888bc98189cac698420fc2
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-03-22 07:40:48 -0400

    NavigationStackAPI initial commit

---
+++ NavigationStackAPI/NavigationStackAPI/NavigationStackAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackAPIApp.swift
+//  NavigationStackAPI
+//
+//  Created by Thomas Cowern on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

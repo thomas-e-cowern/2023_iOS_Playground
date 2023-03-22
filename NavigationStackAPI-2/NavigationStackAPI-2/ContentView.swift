70212a836c9ba528fde3c5ab955bccd00886d6c4
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-03-22 08:44:15 -0400

    update ContentView with dynamic navigation

--- NavigationStackAPI-2/NavigationStackAPI-2/ContentView.swift
+++ NavigationStackAPI-2/NavigationStackAPI-2/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var numbers: [Int] = [2, 3]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack(path: $numbers) {
+            VStack (spacing: 20) {
+                Button("Navigate") {
+                    numbers.append(1)
+                }
+                
+                Button("Random Number") {
+                    let randomInt = Int.random(in: 100...200)
+                    numbers.append(randomInt)
+                }
+            }
+            .navigationDestination(for: Int.self) { value in
+                Text("\(value)")
+            }
         }
-        .padding()
     }
 }
 

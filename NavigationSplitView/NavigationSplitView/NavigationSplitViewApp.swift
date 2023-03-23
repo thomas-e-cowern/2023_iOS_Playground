52054cb7327d46b302885354500b4137c74d39b0
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-03-23 10:13:05 -0400

    NavigationSpliView initial commit

---
+++ NavigationSplitView/NavigationSplitView/NavigationSplitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSplitViewApp.swift
+//  NavigationSplitView
+//
+//  Created by Thomas Cowern on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSplitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

c5fc61b824d4031c98601d1120f1664bd5eed371
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-03-23 10:29:09 -0400

    add GenreDetailView and Genre to ContentView

--- NavigationSplitView/NavigationSplitView/ContentView.swift
+++ NavigationSplitView/NavigationSplitView/ContentView.swift
@@ -7,10 +7,32 @@
 
 import SwiftUI
 
+enum Genre: String, Hashable, CaseIterable {
+    case action = "Action"
+    case horror = "Horror"
+    case fiction = "Fiction"
+    case kids = "Kids"
+    case comedy = "Comedy"
+}
+
+struct GenreListview: View {
+    
+    @Binding var selectedGenre: Genre?
+    
+    var body: some View {
+        List(Genre.allCases, id: \.self) { genre in
+            Text(genre.rawValue)
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var selectedGenre: Genre? = .action
+    
     var body: some View {
         NavigationSplitView {
-            Text("Sidebar")
+            GenreListview(selectedGenre: $selectedGenre)
         } detail: {
             Text("Detail")
         }

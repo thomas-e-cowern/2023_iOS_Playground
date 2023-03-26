e01f7c33f7faec8bf433a489aa9a221f533f3c40
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-03-26 13:04:17 -0400

    VDBNetworkTest

---
+++ VeteranDBNetworkTest/VeteranDBNetworkTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VeteranDBNetworkTest
+//
+//  Created by Thomas Cowern on 3/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    
+    var body: some View {
+        VStack {
+            TextField
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VeteranDBNetworkTest/VeteranDBNetworkTest/VeteranDBNetworkTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeteranDBNetworkTestApp.swift
+//  VeteranDBNetworkTest
+//
+//  Created by Thomas Cowern on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct VeteranDBNetworkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

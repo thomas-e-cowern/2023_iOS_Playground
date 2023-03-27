ee92e6d116698bb741a9eb761e5ac36dcb5af2bf
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-03-27 15:22:01 -0400

    MenuPlayground initial commit

---
+++ MenuPlayground/MenuPlayground/MenuPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuPlaygroundApp.swift
+//  MenuPlayground
+//
+//  Created by Thomas Cowern on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

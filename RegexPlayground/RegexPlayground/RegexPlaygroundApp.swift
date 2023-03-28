02fbd71b687b67f182e1f5f2f3d548503806a380
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-03-28 10:50:06 -0400

    add RegexPlayground project

---
+++ RegexPlayground/RegexPlayground/RegexPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegexPlaygroundApp.swift
+//  RegexPlayground
+//
+//  Created by Thomas Cowern on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct RegexPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

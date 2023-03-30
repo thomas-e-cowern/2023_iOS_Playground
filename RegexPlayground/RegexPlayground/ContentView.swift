5bcc30d37c20f2a17bbd49bf27f09ba219f18f26
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-03-30 10:29:15 -0400

    add Buttons and defaults to ContentView

--- RegexPlayground/RegexPlayground/ContentView.swift
+++ RegexPlayground/RegexPlayground/ContentView.swift
@@ -13,8 +13,26 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            TextField("Enter your regular expression", text: $model.pattern)
-                .foregroundColor(model.isValid ? .primary : .red)
+            HStack {
+                TextField("Enter your regular expression", text: $model.pattern)
+                    .foregroundColor(model.isValid ? .primary : .red)
+                
+                Menu {
+                    Button("Date (dd/mm/yyyy") {
+                        model.pattern = #"(0?[1-9]|[12][0-9]|3[01])([-/.])(0?[1-9]|1[012])\2((?:19|20)\d\d)"#
+                    }
+                    
+                    Button("Email Address") {
+                        model.pattern = #"[:graph:]+@[:graph:]+\.[:graph:]{2,}"#
+                    }
+                    
+                    Button("To Do List") {
+                        model.pattern += #"(\d+)(?:\.|:)? *(.+)"#
+                    }
+                } label: {
+                    Label("Presets", systemImage: "gear")
+                }
+            }
             TextEditor(text: $model.inputString)
                 .padding(5)
                 .border(.quaternary)

dff76e28014c40d30008ca046fa2c2b7d941cb39
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-04-19 14:44:10 -0400

    update textfieds

--- VeteranDBNetworkTest/VeteranDBNetworkTest/ContentView.swift
+++ VeteranDBNetworkTest/VeteranDBNetworkTest/ContentView.swift
@@ -12,14 +12,22 @@ struct ContentView: View {
     @StateObject private var loginVM = LoginViewModel()
     
     var body: some View {
-        VStack {
+        VStack (spacing: 10) {
             TextField("Username", text: $loginVM.username)
+                .padding(10)
+                .border(.black)
+                .textInputAutocapitalization(.never)
+                .frame(width: 200, height: 45)
             SecureField("Password", text: $loginVM.password)
+                .padding(10)
+                .border(.black)
+                .textInputAutocapitalization(.never)
+                .frame(width: 200, height: 45)
             Button("Login") {
                 loginVM.login()
             }
+            .buttonStyle(.automatic)
         }
-        .padding(10)
     }
 }
 
--- VeteranDBNetworkTest/VeteranDBNetworkTest/Services/Webservice.swift
+++ VeteranDBNetworkTest/VeteranDBNetworkTest/Services/Webservice.swift
@@ -70,21 +70,25 @@ class Webservice {
     
     
     func login(username: String, password: String, completion: @escaping (Result<String, AuthenticationError>) -> Void) {
-        print(username)
-        print(password)
-//        guard let url = URL(string: "https://api.veterandb.com/v1/account/login") else {
-//            completion(.failure(.custom(errorMessage: "URL is not correct")))
-//            return
-//        }
-//
-//        let body = LoginRequestBody(username: username, password: password)
-//
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "POST"
-//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.addValue("Bearer 34388ecc8cc4e82da33a288cb103b758612ab0a338b135e2d449c200d0e41b2390c86970f991f03ae9505adee887be38a0a081898282dfc6a6bcba6dbe1f8de868ad25d0849fe0968ba6a9e656a82b5736ee54013ee3114aa097825cecac55155f3e537b60c9441f4304c15828c634261c1b1931778b719d4474d97694515153", forHTTPHeaderField: "Authorization")
-//        request.addValue("veterandb-access=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Nzk4NTA2MTQsImV4cCI6MTY4MjQ0MjYxNH0.mAaupPbTSTLoRIohC8ZYu-WFJF1n2MG8n2gdDvkFA98", forHTTPHeaderField: "Cookie")
-//        request.httpBody = try? JSONEncoder().encode(body)
+
+        guard let url = URL(string: "https://api.veterandb.com/v1/account/login") else {
+            completion(.failure(.custom(errorMessage: "URL is not correct")))
+            return
+        }
+        
+        let parameters = "{\n\t\"email\": \"\(username)\",\n\t\"password\": \"\(password)\"\n}"
+
+        let postData = parameters.data(using: .utf8)
+        
+        var request = URLRequest(url: url, timeoutInterval: Double.infinity)
+
+        request.addValue("Bearer 34388ecc8cc4e82da33a288cb103b758612ab0a338b135e2d449c200d0e41b2390c86970f991f03ae9505adee887be38a0a081898282dfc6a6bcba6dbe1f8de868ad25d0849fe0968ba6a9e656a82b5736ee54013ee3114aa097825cecac55155f3e537b60c9441f4304c15828c634261c1b1931778b719d4474d97694515153", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("veterandb-access=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Nzk4NTA2MTQsImV4cCI6MTY4MjQ0MjYxNH0.mAaupPbTSTLoRIohC8ZYu-WFJF1n2MG8n2gdDvkFA98", forHTTPHeaderField: "Cookie")
+
+        request.httpMethod = "POST"
+        request.httpBody = postData
+
 //
 //        URLSession.shared.dataTask(with: request) { (data, response, error) in
 //
@@ -107,30 +111,24 @@ class Webservice {
 //            print("Data: \(loginResponse)")
 //
 //        }.resume()
-        let parameters = "{\n\t\"email\": \"\(username)\",\n\t\"password\": \"\(password)\"\n}"
-//        let parameters = "{ \"email\": "\(username)", \"password": "\(password)" }"
-        print(parameters)
-        let postData = parameters.data(using: .utf8)
 
-        var request = URLRequest(url: URL(string: "https://api.veterandb.com/v1/account/login")!,timeoutInterval: Double.infinity)
-        request.addValue("Bearer 34388ecc8cc4e82da33a288cb103b758612ab0a338b135e2d449c200d0e41b2390c86970f991f03ae9505adee887be38a0a081898282dfc6a6bcba6dbe1f8de868ad25d0849fe0968ba6a9e656a82b5736ee54013ee3114aa097825cecac55155f3e537b60c9441f4304c15828c634261c1b1931778b719d4474d97694515153", forHTTPHeaderField: "Authorization")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("veterandb-access=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Nzk4NTA2MTQsImV4cCI6MTY4MjQ0MjYxNH0.mAaupPbTSTLoRIohC8ZYu-WFJF1n2MG8n2gdDvkFA98", forHTTPHeaderField: "Cookie")
-
-        request.httpMethod = "POST"
-        request.httpBody = postData
 
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
-          guard let data = data else {
-            print(String(describing: error))
-            return
-          }            
-            let login = try! JSONDecoder().decode(LoginData.self, from: data)
             
-            print("Data: \(login.data.status)")
-            print("Data: \(login.data.access_token)")
+            guard let data = data else {
+              completion(.failure(.custom(errorMessage: "No data")))
+              return
+            }
             
-            completion(.success(login.data.access_token))
+            guard let login = try? JSONDecoder().decode(LoginResponse.self, from: data) else {
+                completion(.failure(.invalidCredentials))
+                return
+            }
+            
+            print("Data: \(login.status)")
+            print("Data: \(login.access_token)")
+            
+            completion(.success(login.access_token))
         }
 
         task.resume()

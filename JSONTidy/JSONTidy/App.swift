192fddb4b536f8e73aafba5612dae18268bf3694
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-20 12:01:21 -0400

    update run method

--- JSONTidy/JSONTidy/App.swift
+++ JSONTidy/JSONTidy/App.swift
@@ -19,7 +19,25 @@ struct App: ParsableCommand {
     var file: String
     
     func run() {
-        print("Hello from Swift Argument Parser!")
+        let url = URL(fileURLWithPath: file)
+
+            guard let contents = try? Data(contentsOf: url) else {
+                print("Failed to read input file \(file)")
+                return
+            }
+
+            guard let json = try? JSONSerialization.jsonObject(with: contents, options: .fragmentsAllowed) else {
+                print("Failed to parse JSON in input file \(file)")
+                return
+            }
+
+            do {
+                let newData = try JSONSerialization.data(withJSONObject: json)
+                let outputString = String(decoding: newData, as: UTF8.self)
+                print(outputString)
+            } catch {
+                print("Failed to create JSON output.")
+            }
     }
 }
 

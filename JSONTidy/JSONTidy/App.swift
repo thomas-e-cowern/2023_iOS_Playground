f13e56287fdd9ec6be1ca354822b99464416578d
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-20 11:49:46 -0400

    started over

---
+++ JSONTidy/JSONTidy/App.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  JSONTidy
+//
+//  Created by Thomas Cowern on 5/20/23.
+//
+import ArgumentParser
+import Foundation
+
+@main
+struct App: ParsableCommand {
+    func run() {
+        print("Hello from Swift Argument Parser!")
+    }
+}
+

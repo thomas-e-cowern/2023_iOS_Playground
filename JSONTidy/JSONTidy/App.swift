de3cba8499e1ebb8df7a651d09af74c7aeffc0ac
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-20 11:56:04 -0400

    add Flag to file

--- JSONTidy/JSONTidy/App.swift
+++ JSONTidy/JSONTidy/App.swift
@@ -9,6 +9,15 @@ import Foundation
 
 @main
 struct App: ParsableCommand {
+    @Flag(name: .shortAndLong, help: "Neatly formats the output")
+    var prettify = false
+
+    @Flag(name: .shortAndLong, help: "Sort keys alphabetically.")
+    var sort = false
+
+    @Argument(help: "The filename you want to process.")
+    var file: String
+    
     func run() {
         print("Hello from Swift Argument Parser!")
     }

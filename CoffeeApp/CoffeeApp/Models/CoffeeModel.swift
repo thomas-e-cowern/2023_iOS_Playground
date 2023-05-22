cd89ee33a3818e1bfea72132fca363765774ef65
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-22 16:27:06 -0400

    update CoffeeModel

---
+++ CoffeeApp/CoffeeApp/Models/CoffeeModel.swift
@@ -0,0 +1,24 @@
+//
+//  CoffeeModel.swift
+//  CoffeeApp
+//
+//  Created by Thomas Cowern on 5/22/23.
+//
+
+import Foundation
+
+@MainActor
+class CoffeeModel: ObservableObject {
+    
+    let webService: WebService
+    @Published private(set) var orders: [Order] = []
+    
+    init(webService: WebService) {
+        self.webService = webService
+    }
+    
+    func populateOrders() async throws {
+        orders = try await webService.getOrders()
+    }
+    
+}

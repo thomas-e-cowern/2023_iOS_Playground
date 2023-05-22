1c7115ea8728d934c8b97747f75457e21810cdcf
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-22 10:22:03 -0400

    netowrking sandbox initial commit

---
+++ NetworkingSandbox/NetworkingSandbox/NetworkingSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingSandboxApp.swift
+//  NetworkingSandbox
+//
+//  Created by Thomas Cowern on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4151af9eb1bfeaed86c7d3f3716341031fc54adb
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-22 10:38:42 -0400

    add helpers folder and constants file

---
+++ NetworkingSandbox/NetworkingSandbox/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  NetworkingSandbox
+//
+//  Created by Thomas Cowern on 5/22/23.
+//
+
+import Foundation
+
+struct Endpoint {
+    var url: URL
+
+    static let headlines = Endpoint(url: URL(string: "https://hws.dev/headlines.json")!)
+    static let messages = Endpoint(url: URL(string: "https://hws.dev/messages.json")!)
+}

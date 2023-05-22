978cf4c89d85568ab2adda885de529be54bd2c67
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-22 15:48:21 -0400

    coffee app initial commit

---
+++ CoffeeApp/CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Thomas Cowern on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

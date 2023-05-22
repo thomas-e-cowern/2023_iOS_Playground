240cd1bdd4556045099f57786dd232cd2a8a213e
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-22 16:12:26 -0400

    add WebService

--- CoffeeApp/CoffeeApp/Services/WebService.swift
+++ CoffeeApp/CoffeeApp/Services/WebService.swift
@@ -7,10 +7,30 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    case badRequest
+    case decodingError
+    case badUrl
+}
+
 class WebService {
     
     func getOrders() async throws -> [Order] {
         
+        guard let url = URL(string: "https://island-bramble.glitch.me/orders") else {
+            throw NetworkError.badUrl
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkError.badRequest
+        }
+        
+        guard let orders = try? JSONDecoder().decode([Order].self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return orders
     }
     
 }

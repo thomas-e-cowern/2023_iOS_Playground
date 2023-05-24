66d4510859c3aa24a735217b9bb184d0ca478e14
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-23 21:14:23 -0400

    update routes

--- hello-vapor/Sources/App/routes.swift
+++ hello-vapor/Sources/App/routes.swift
@@ -8,4 +8,11 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("movies", ":genre") { req async throws -> String in
+        guard let genre = req.parameters.get("genre") else {
+            throw Abort(.badRequest)
+        }
+        return "All movies of genre: \(String(describing: genre))"
+    }
 }

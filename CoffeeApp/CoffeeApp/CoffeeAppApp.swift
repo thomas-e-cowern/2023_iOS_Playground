ec755361ec8a2e1f199e8f4abb959484ec60fbf7
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-24 15:25:19 -0400

    update coffeappapp

--- CoffeeApp/CoffeeApp/CoffeeAppApp.swift
+++ CoffeeApp/CoffeeApp/CoffeeAppApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct CoffeeAppApp: App {
+    
+    @StateObject private var model: CoffeeModel
+    
+    init() {
+        let webService = WebService()
+        _model = StateObject(wrappedValue: CoffeeModel(webService: webService))
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+            ContentView().environmentObject(model)
         }
     }
 }

bfbd56591356fdad55d699ffa54a13a19b928bbb
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-25 14:41:05 -0400

    update movies controller

--- hello-vapor/Sources/App/Controllers/MoviesController.swift
+++ hello-vapor/Sources/App/Controllers/MoviesController.swift
@@ -10,5 +10,25 @@ import Vapor
 
 struct MoviesController: RouteCollection {
     
+    func boot(routes: RoutesBuilder) throws {
+        let movies = routes.grouped("movies")
+        movies.get(use: index)
+        
+        movies.get(":movieId", use: show)
+        
+    }
     
+    func index(req: Request) async throws -> String {
+        return "Index"
+    }
+    
+    func show(req: Request) async throws -> String {
+        guard let movieId = req.parameters.get("movieId") else {
+            throw Abort(.internalServerError)
+        }
+        if movieId == "66" {
+            return "This is movie \(movieId)"
+        }
+        return "This movie was not found"
+    }
 }
--- hello-vapor/Sources/App/routes.swift
+++ hello-vapor/Sources/App/routes.swift
@@ -1,55 +1,57 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
     
-    app.get("hotels") { req async throws in
-        let hotelQuery = try req.query.decode(HotelQuery.self)
-        return hotelQuery
-    }
-    
-    app.post("movies") { req async throws in
-        let movie = try req.content.decode(Movie.self)
-        return movie
-    }
-    
-    app.get("movies", ":genre") { req async throws -> String in
-        guard let genre = req.parameters.get("genre") else {
-            throw Abort(.badRequest)
-        }
-        return "All movies of genre: \(String(describing: genre))"
-    }
-    
-    app.get("movies", ":genre", ":year") { req async throws -> String in
-        guard let genre = req.parameters.get("genre"),
-              let year = req.parameters.get("year")
-        else {
-            throw Abort(.badRequest)
-        }
-        
-        return "All movies of genre \(genre) in \(year)"
-    }
-    
-    app.get("customers", ":customerId") { req async throws -> String in
-    
-        guard let customerId = req.parameters.get("customerId", as: Int.self) else {
-            throw Abort(.badRequest)
-        }
-        
-        return "\(customerId)"
-    }
-    
-    app.get("movies") { req async in
-        [
-            Movie(title: "The Big Year", year: 2015),
-            Movie(title: "Frankenstien", year: 1993),
-            Movie(title: "Bill and Ted", year: 1990)
-        ]
-    }
+    try app.register(collection: MoviesController())
+//    app.get { req async in
+//        "It works!"
+//    }
+//
+//    app.get("hello") { req async -> String in
+//        "Hello, world!"
+//    }
+//
+//    app.get("hotels") { req async throws in
+//        let hotelQuery = try req.query.decode(HotelQuery.self)
+//        return hotelQuery
+//    }
+//
+//    app.post("movies") { req async throws in
+//        let movie = try req.content.decode(Movie.self)
+//        return movie
+//    }
+//
+//    app.get("movies", ":genre") { req async throws -> String in
+//        guard let genre = req.parameters.get("genre") else {
+//            throw Abort(.badRequest)
+//        }
+//        return "All movies of genre: \(String(describing: genre))"
+//    }
+//
+//    app.get("movies", ":genre", ":year") { req async throws -> String in
+//        guard let genre = req.parameters.get("genre"),
+//              let year = req.parameters.get("year")
+//        else {
+//            throw Abort(.badRequest)
+//        }
+//
+//        return "All movies of genre \(genre) in \(year)"
+//    }
+//
+//    app.get("customers", ":customerId") { req async throws -> String in
+//
+//        guard let customerId = req.parameters.get("customerId", as: Int.self) else {
+//            throw Abort(.badRequest)
+//        }
+//
+//        return "\(customerId)"
+//    }
+//
+//    app.get("movies") { req async in
+//        [
+//            Movie(title: "The Big Year", year: 2015),
+//            Movie(title: "Frankenstien", year: 1993),
+//            Movie(title: "Bill and Ted", year: 1990)
+//        ]
+//    }
 }

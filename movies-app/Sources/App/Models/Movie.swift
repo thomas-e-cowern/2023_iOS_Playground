475c10e715cdda5a0a4ecac163d9b5958ee45f89
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-27 22:42:04 -0400

    add models folder and movie model

---
+++ movies-app/Sources/App/Models/Movie.swift
@@ -0,0 +1,28 @@
+//
+//  Movie.swift
+//  
+//
+//  Created by Thomas Cowern on 5/27/23.
+//
+
+import Foundation
+import Vapor
+import Fluent
+
+final class Movie: Model {
+    
+    static let schema: String = "movies"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "title")
+    var title: String
+    
+    init() {}
+    
+    init(id: UUID? = nil, title: String) {
+        self.id = id
+        self.title = title
+    }
+}

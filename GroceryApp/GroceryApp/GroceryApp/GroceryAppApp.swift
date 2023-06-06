1a47c0589673e896b8bc9191bd1bf7e953121896
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-06 11:00:01 -0400

    add grocery-app-server and grocery app client

---
+++ GroceryApp/GroceryApp/GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

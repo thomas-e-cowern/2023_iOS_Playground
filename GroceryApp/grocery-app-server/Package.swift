561398f38b4a63aba41075683cfd8db90242b68f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-06 11:20:48 -0400

    update package in grocery-app-serverr

--- GroceryApp/grocery-app-server/Package.swift
+++ GroceryApp/grocery-app-server/Package.swift
@@ -9,6 +9,9 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
     ],
     targets: [
         .executableTarget(

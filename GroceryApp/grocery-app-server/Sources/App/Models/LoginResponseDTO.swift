05c0afb83302c84be876ae30f6b121a024b867e9
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-07 10:31:40 -0400

    add login response dto

---
+++ GroceryApp/grocery-app-server/Sources/App/Models/LoginResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  LoginResponseDTO.swift
+//  
+//
+//  Created by Thomas Cowern on 6/7/23.
+//
+
+import Foundation
+import Vapor
+
+struct LoginResponseDTO: Codable {
+    
+    let error: Bool
+    var reason: String? = nil
+    let token: String?
+    let userId: UUID
+    
+}

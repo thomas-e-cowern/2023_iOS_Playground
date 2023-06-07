84569a8b9c0ac846dc162664cc0fa64ff5a55f34
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-07 10:14:23 -0400

    add auth payload to models

---
+++ GroceryApp/grocery-app-server/Sources/App/Models/AuthPayload.swift
@@ -0,0 +1,30 @@
+//
+//  AuthPayload.swift
+//  
+//
+//  Created by Thomas Cowern on 6/7/23.
+//
+
+import Foundation
+import JWT
+
+struct AuthPayload: JWTPayload {
+    
+    typealias Payload = AuthPayload
+    
+    enum CodingKeys: String, CodingKey {
+        case subject = "sub"
+        case expiration = "exp"
+        case userId = "uid"
+    }
+    
+    var subject: SubjectClaim
+    var expiration: ExpirationClaim
+    var userId: UUID
+    
+    func verify(using signer: JWTKit.JWTSigner) throws {
+        try self.expiration.verifyNotExpired()
+    }
+    
+    
+}

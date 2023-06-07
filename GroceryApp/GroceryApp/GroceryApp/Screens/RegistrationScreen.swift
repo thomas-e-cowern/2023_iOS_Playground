e2235768dccb3952b54574b792e7a0c63f1e8c2f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-07 11:08:10 -0400

    add screens folder and registration screen

---
+++ GroceryApp/GroceryApp/GroceryApp/Screens/RegistrationScreen.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationScreen.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 6/7/23.
+//
+
+import SwiftUI
+
+struct RegistrationScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegistrationScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationScreen()
+    }
+}

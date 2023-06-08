525b0e332dfe0a6f2e7f8807ded22a4e11bcbe42
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-08 11:52:44 -0400

    update grocery model register method

--- GroceryApp/GroceryApp/GroceryApp/Models/GroceryModel.swift
+++ GroceryApp/GroceryApp/GroceryApp/Models/GroceryModel.swift
@@ -9,11 +9,16 @@ import Foundation
 
 class GroceryModel: ObservableObject {
     
+    let httpClient = HTTPClient()
+    
     func register(username: String, password: String) async throws -> Bool {
         
         let registerData = ["username": username, "password": password]
         
-        let resource = Resource(url: <#T##URL#>, method: .post(try JSONEncoder().encode(registerData)), modelType: RegisterResponseDTO.self)
+        let resource = try Resource(url: Constants.Urls.register, method: .post(JSONEncoder().encode(registerData)), modelType: RegisterResponseDTO.self)
+        let registerResponseDTO = try await httpClient.load(resource)
+        
+        return registerResponseDTO.error
     }
     
 }
--- GroceryApp/GroceryApp/GroceryApp/Utilities/Constants.swift
+++ GroceryApp/GroceryApp/GroceryApp/Utilities/Constants.swift
@@ -12,7 +12,7 @@ struct Constants {
     private static let baseUrlPath = "http://127.0.0.1:8080"
     
     struct Urls {
-        static let register = URL(string: "\(baseUrlPath)/register")
+        static let register = URL(string: "\(baseUrlPath)/register")!
     }
     
 }

e6642225eca120ba0a9028704cd8813af3f68f24
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-09 11:24:14 -0400

    add login screen

---
+++ GroceryApp/GroceryApp/GroceryApp/Screens/LoginScreen.swift
@@ -0,0 +1,51 @@
+//
+//  LoginScreen.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 6/9/23.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var errorMessage: String = ""
+    
+    private var isFormValid: Bool {
+        !username.isEmptyOrWhiteSpace && !password.isEmptyOrWhiteSpace && (password.count >= 6 && password.count <= 10)
+    }
+    
+    var body: some View {
+        Form {
+            TextField("Username", text: $username)
+                .textInputAutocapitalization(.never)
+            SecureField("Password", text: $password)
+            
+            HStack {
+                Button("Log In") {
+                    Task {
+//                        await register()
+                    }
+                }.buttonStyle(.borderless)
+                    .disabled(!isFormValid)
+            }
+            
+            Text(errorMessage)
+            
+        }
+        .navigationTitle("Registration")
+    }
+    
+    private func login() async {
+        
+    }
+}
+
+struct LoginScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginScreen()
+            .environmentObject(GroceryModel())
+    }
+}

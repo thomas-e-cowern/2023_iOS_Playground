d1afb1a093fe8053a41706d7d013fc738fbee1c7
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-09 12:18:19 -0400

    add navigation stack to grocery app app

--- GroceryApp/GroceryApp/GroceryApp/GroceryAppApp.swift
+++ GroceryApp/GroceryApp/GroceryApp/GroceryAppApp.swift
@@ -11,11 +11,25 @@ import SwiftUI
 struct GroceryAppApp: App {
     
     @StateObject private var model = GroceryModel()
+    @StateObject private var appState = AppState()
     
     var body: some Scene {
         WindowGroup {
-            RegistrationScreen()
-                .environmentObject(model)
+            NavigationStack(path: $appState.routes) {
+                RegistrationScreen()
+                    .navigationDestination(for: Route.self) { route in
+                        switch route {
+                        case .register:
+                            RegistrationScreen()
+                        case .login:
+                            LoginScreen()
+                        case .groceryCategoryList:
+                            Text("Grocery Category List")
+                        }
+                    }
+            }
+            .environmentObject(model)
+            .environmentObject(appState)
         }
     }
 }

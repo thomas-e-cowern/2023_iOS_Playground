e724edfd6b1fab91b0994d9bb751f481ce1c70dc
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-14 11:21:42 -0400

    update register response dto

--- GroceryAppSharedDTO/Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
+++ GroceryAppSharedDTO/Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-struct LoginResponseDTO: Codable {
-    let error: Bool
-    var reason: String? = nil
-    var token: String? = nil
-    var userId: UUID? = nil
+public struct LoginResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    public var token: String? = nil
+    public var userId: UUID? = nil
+    
+    public init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil) {
+        self.error = error
+        self.reason = reason
+        self.token = token
+        self.userId = userId
+    }
 }

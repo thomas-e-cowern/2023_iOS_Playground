8aa182ac8abe67f09ef885116b501ef652582439
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-15 11:13:38 -0400

    add CreateGroceryCategoryTableMigration

---
+++ GroceryApp/grocery-app-server/Sources/App/Migrations/CreateGroceryCategoryTableMigration.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Thomas Cowern on 6/15/23.
+//
+
+import Foundation
+import Fluent
+
+class CreateGroceryCategoryTableMigration: AsyncMigration {
+    
+    func revert(on database: FluentKit.Database) async throws {
+        <#code#>
+    }
+    
+    func prepare(on database: FluentKit.Database) async throws {
+        <#code#>
+    }
+
+}
+

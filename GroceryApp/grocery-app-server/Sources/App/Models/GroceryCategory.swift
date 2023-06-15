ca7acfe17c97de781795addca419fcad6e5df64b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-15 11:48:43 -0400

    update grocery category

---
+++ GroceryApp/grocery-app-server/Sources/App/Models/GroceryCategory.swift
@@ -0,0 +1,41 @@
+//
+//  GroceryCategory..swift
+//  
+//
+//  Created by Thomas Cowern on 6/15/23.
+//
+
+import Foundation
+import Vapor
+import Fluent
+
+final class GroceryCategory: Model, Content, Validatable {
+
+    static var schema = "grocery_categories"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "title")
+    var title: String
+    
+    @Field(key: "color_code")
+    var colorCode: String
+    
+    @Parent(key: "user_id")
+    var user: User
+    
+    init() { }
+    
+    init(id: UUID? = nil, title: String, colorCode: String, userId: UUID) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+        self.$user.id = userId
+    }
+    
+    static func validations(_ validations: inout Vapor.Validations) {
+        validations.add("title", as: String.self, is: !.empty, customFailureDescription: "Title cannot be empty")
+        validations.add("colorCode", as: String.self, is: !.empty, customFailureDescription: "Color code cannot be empty")
+    }
+}

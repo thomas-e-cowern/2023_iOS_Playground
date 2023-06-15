6f6ab16d3d2e7b63c83b14c452398f397b10b5e8
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-15 12:03:05 -0400

    update grocery controller with routes

--- GroceryApp/grocery-app-server/Sources/App/Controllers/GroceryController.swift
+++ GroceryApp/grocery-app-server/Sources/App/Controllers/GroceryController.swift
@@ -9,6 +9,23 @@ import Foundation
 import Vapor
 import Fluent
 
-class GroceryController; RouteCollection {
+class GroceryController: RouteCollection {
     
+    func boot(routes: RoutesBuilder) throws {
+        
+        // /api/users/:userId
+        let api = routes.grouped("api", "users", ":userId")
+        // POST: saving groceery category
+        // /api/users/:userId/grocery-categories
+        api.post("grocery-categories", use: saveGroceryCategory)
+    }
+    
+    func saveGroceryCategory(req: Request) async throws -> String {
+        
+        // DTO for request
+        
+        // DTO for reqponse
+        
+        return "Ok"
+    }
 }

1882a5c07e55be03d51992c5b660a9266d207266
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-15 11:57:21 -0400

    add grocery controller

---
+++ GroceryApp/grocery-app-server/Sources/App/Controllers/GroceryController.swift
@@ -0,0 +1,14 @@
+//
+//  GroceryController.swift
+//  
+//
+//  Created by Thomas Cowern on 6/15/23.
+//
+
+import Foundation
+import Vapor
+import Fluent
+
+class GroceryController; RouteCollection {
+    
+}

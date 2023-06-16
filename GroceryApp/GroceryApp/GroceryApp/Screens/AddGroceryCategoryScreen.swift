6bcadc820cefe1e470dbf367535a042c3778936f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-16 09:05:58 -0400

    update add grocery category screen with toolbar and methods

--- GroceryApp/GroceryApp/GroceryApp/Screens/AddGroceryCategoryScreen.swift
+++ GroceryApp/GroceryApp/GroceryApp/Screens/AddGroceryCategoryScreen.swift
@@ -12,15 +12,45 @@ struct AddGroceryCategoryScreen: View {
     @State private var title: String = ""
     @State private var colorCode: String = Constants.defaultColor
     
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         Form {
             TextField("Title", text: $title)
+            ColorSelectorView(colorCode: $colorCode)
         }
+        .navigationTitle("New Category")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button("Close") {
+                    dismiss()
+                }
+            }
+            
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Save") {
+                    Task {
+                        await saveGroceryCategory()
+                    }
+                }
+                .disabled(isFormValid)
+            }
+        }
+    }
+    
+    private func saveGroceryCategory() async {
+        
+    }
+    
+    private var isFormValid: Bool {
+        title.isEmptyOrWhiteSpace
     }
 }
 
 struct AddGroceryCategoryScreen_Previews: PreviewProvider {
     static var previews: some View {
-        AddGroceryCategoryScreen()
+        NavigationStack {
+            AddGroceryCategoryScreen()
+        }
     }
 }

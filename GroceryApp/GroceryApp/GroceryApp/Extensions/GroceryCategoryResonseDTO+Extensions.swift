8e8c1d84fe2457e86caa434c6db56831d179d886
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-16 11:16:54 -0400

    add groceery category resonse dto extension

---
+++ GroceryApp/GroceryApp/GroceryApp/Extensions/GroceryCategoryResonseDTO+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  GroceryCategoryResonseDTO+Extensions.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 6/16/23.
+//
+
+import Foundation
+import GroceryAppSharedDTO
+
+extension GroceryCategoryResonseDTO: Identifiable {
+
+}

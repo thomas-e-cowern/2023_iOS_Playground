fb6ef18f3dcd29bcabb07dcd6eaa5f65839b5d3a
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-16 08:41:22 -0400

    add views folder and color selector view

---
+++ GroceryApp/GroceryApp/GroceryApp/Views/ColorSelectorView.swift
@@ -0,0 +1,20 @@
+//
+//  ColorSelectorView.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 6/16/23.
+//
+
+import SwiftUI
+
+struct ColorSelectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorSelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorSelectorView()
+    }
+}

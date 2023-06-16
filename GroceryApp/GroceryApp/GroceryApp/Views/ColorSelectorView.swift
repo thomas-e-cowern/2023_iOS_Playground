b8a789383fab9680d96e293229c348ffd72215e3
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-16 08:44:38 -0400

    add colors to color selector view

--- GroceryApp/GroceryApp/GroceryApp/Views/ColorSelectorView.swift
+++ GroceryApp/GroceryApp/GroceryApp/Views/ColorSelectorView.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+enum Colors: String, CaseIterable {
+    case green = "#2ecc71"
+    case red = "#e74c3c"
+    case blue = "#3498db"
+    case purple = "#9b59b6"
+    case yellow = "#f1c40f"
+}
+
 struct ColorSelectorView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)

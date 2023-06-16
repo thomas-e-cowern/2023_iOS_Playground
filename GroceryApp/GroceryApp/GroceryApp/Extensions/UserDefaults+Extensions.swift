f9cd1a368968a870f0e5ceabc55151c8d978b738
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-16 10:22:05 -0400

    add user defaults extions to extensions folder

---
+++ GroceryApp/GroceryApp/GroceryApp/Extensions/UserDefaults+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  UserDefaults+Extensions.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 6/16/23.
+//
+
+import Foundation
+
+extension UserDefaults {
+    var userId: UUID? {
+        get {
+            guard let userIdAsString = string(forKey: "userId") else {
+                return nil
+            }
+            
+            return UUID(uuidString: userIdAsString)
+        }
+    }
+}

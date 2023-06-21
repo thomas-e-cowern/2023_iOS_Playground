445de7bd3a0a1cb7716d39ea4653f152b14d31bb
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-21 09:29:09 -0400

    add grocery item table migration

---
+++ GroceryApp/grocery-app-server/Sources/App/Migrations/CreateGroceryItemTableMigration.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Thomas Cowern on 6/21/23.
+//
+
+import Foundation
+import Fluent
+
+class CreateGroceryItemTableMigration: AsyncMigration {
+    
+    func revert(on database: FluentKit.Database) async throws {
+        try await database.schema("grocery_items")
+            .delete()
+    }
+    
+    func prepare(on database: FluentKit.Database) async throws {
+        try await database.schema("grocery_items")
+            .id()
+            .field("title", .string, .required)
+            .field("price", .double, .required)
+            .field("quantity", .int, .required)
+            .field("userId", .uuid, .required, .references("grocery_categories", "id", onDelete: .cascade))
+            .create()
+    }
+}

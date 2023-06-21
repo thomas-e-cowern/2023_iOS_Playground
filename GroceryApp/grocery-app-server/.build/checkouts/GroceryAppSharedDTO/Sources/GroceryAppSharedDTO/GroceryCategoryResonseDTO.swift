bf71fb260da580560af0f91b80acbadbdcbe4f99
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-21 09:41:14 -0400

    run grocery item migration

---
+++ GroceryApp/grocery-app-server/.build/checkouts/GroceryAppSharedDTO/Sources/GroceryAppSharedDTO/GroceryCategoryResonseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  GroceryCategoryResonseDTO.swift
+//
+//
+//  Created by Thomas Cowern on 6/15/23.
+//
+
+import Foundation
+
+public struct GroceryCategoryResonseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let colorCode: String
+    
+    public init(id: UUID, title: String, colorCode: String) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+    }
+    
+}

3c522a6110edd40c3cd8bc4d2564b09f7464758c
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-22 10:06:16 -0400

    add grocery item response dto + extensions

---
+++ GroceryApp/GroceryApp/GroceryApp/Extensions/GroceryItemResponseDTO+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  GroceryItemResponseDTO+Extensions.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 6/22/23.
+//
+
+import Foundation
+import GroceryAppSharedDTO
+
+extension GroceryItemResponseDTO: Identifiable {
+    
+
+}

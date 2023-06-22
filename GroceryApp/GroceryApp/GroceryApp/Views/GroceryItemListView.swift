d7462102110508f080970afb55fc553d11c05ac5
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-22 10:04:09 -0400

    update item list view

--- GroceryApp/GroceryApp/GroceryApp/Views/GroceryItemListView.swift
+++ GroceryApp/GroceryApp/GroceryApp/Views/GroceryItemListView.swift
@@ -6,15 +6,23 @@
 //
 
 import SwiftUI
+import GroceryAppSharedDTO
 
 struct GroceryItemListView: View {
+    
+    let groceryItems: [GroceryItemResponseDTO]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(groceryItems) { item in
+                /*@START_MENU_TOKEN@*/Text(item.title)/*@END_MENU_TOKEN@*/
+            } 
+        }
     }
 }
 
 struct GroceryItemListView_Previews: PreviewProvider {
     static var previews: some View {
-        GroceryItemListView()
+        GroceryItemListView(groceryItems: [])
     }
 }

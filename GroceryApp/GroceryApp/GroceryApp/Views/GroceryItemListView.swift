cd271e4dee12e708d956befa7cd1b4d2ecca61fd
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-22 10:00:47 -0400

    add grocery item list view

---
+++ GroceryApp/GroceryApp/GroceryApp/Views/GroceryItemListView.swift
@@ -0,0 +1,20 @@
+//
+//  GroceryItemListView.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 6/22/23.
+//
+
+import SwiftUI
+
+struct GroceryItemListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GroceryItemListView_Previews: PreviewProvider {
+    static var previews: some View {
+        GroceryItemListView()
+    }
+}

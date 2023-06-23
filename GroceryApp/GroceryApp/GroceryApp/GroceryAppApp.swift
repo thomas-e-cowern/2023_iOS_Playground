2a15eb68e755e0f3d1aea48957c056fc0a140200
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-23 08:57:58 -0400

    updated app to fix login start

--- GroceryApp/GroceryApp/GroceryApp/GroceryAppApp.swift
+++ GroceryApp/GroceryApp/GroceryApp/GroceryAppApp.swift
@@ -14,21 +14,31 @@ struct GroceryAppApp: App {
     @StateObject private var appState = AppState()
     
     var body: some Scene {
+        
+        let defaults = UserDefaults.standard
+        let token = defaults.string(forKey: "authToken")
+        
         WindowGroup {
             NavigationStack(path: $appState.routes) {
-                RegistrationScreen()
-                    .navigationDestination(for: Route.self) { route in
-                        switch route {
-                        case .register:
-                            RegistrationScreen()
-                        case .login:
-                            LoginScreen()
-                        case .groceryCategoryList:
-                            GroceryCategoryListScreen()
-                        case .groceryCategoryDetail(let groceryCategory):
-                            GroceryDetailScreen(groceryCategory: groceryCategory)
-                        }
+                Group {
+                    if token == nil {
+                        RegistrationScreen()
+                    } else {
+                        GroceryCategoryListScreen()
                     }
+                }
+                .navigationDestination(for: Route.self) { route in
+                    switch route {
+                    case .register:
+                        RegistrationScreen()
+                    case .login:
+                        LoginScreen()
+                    case .groceryCategoryList:
+                        GroceryCategoryListScreen()
+                    case .groceryCategoryDetail(let groceryCategory):
+                        GroceryDetailScreen(groceryCategory: groceryCategory)
+                    }
+            }
             }
             .environmentObject(model)
             .environmentObject(appState)

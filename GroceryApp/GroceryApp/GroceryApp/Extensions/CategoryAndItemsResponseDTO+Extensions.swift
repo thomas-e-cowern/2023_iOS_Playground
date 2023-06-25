98c626c6924a7deb14c7153f2e2acdd84e9dc3d2
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-25 13:54:49 -0400

    add category and items response dto extension

---
+++ GroceryApp/GroceryApp/GroceryApp/Extensions/CategoryAndItemsResponseDTO+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryAndItemsResponseDTO+Extensions.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 6/25/23.
+//
+
+import Foundation
+import GroceryAppSharedDTO
+
+extension CategoryAndItemsResponseDTO: Identifiable {
+    
+}

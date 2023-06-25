91751fdeb02a01b8e67d25ed67c5f4724c029ba1
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-25 13:33:54 -0400

    add shopping list screen

---
+++ GroceryApp/GroceryApp/GroceryApp/Screens/ShoppingListScreen.swift
@@ -0,0 +1,36 @@
+//
+//  ShoppingListScreen.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 6/25/23.
+//
+
+import SwiftUI
+
+struct ShoppingListScreen: View {
+    
+    @EnvironmentObject private var model: GroceryModel
+    
+    var body: some View {
+        VStack {
+            List {
+                ForEach(model.groceryCategories) { groceryCategory in
+                    NavigationLink(value: Route.groceryCategoryDetail(groceryCategory)) {
+                        HStack {
+                            Circle()
+                                .fill(Color.fromHex(groceryCategory.colorCode))
+                                .frame(width: 25)
+                            Text(groceryCategory.title)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ShoppingListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ShoppingListScreen()
+    }
+}

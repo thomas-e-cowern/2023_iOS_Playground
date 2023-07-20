95c9a4aea98a83bef57da1c12cb81484cb7da4f9
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-20 09:11:02 -0400

    add movie list screen

---
+++ SwiftDataMovie/SwiftDataMovie/Screens/MovieListScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListScreen.swift
+//  SwiftDataMovie
+//
+//  Created by Thomas Cowern on 7/20/23.
+//
+
+import SwiftUI
+
+struct MovieListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListScreen()
+}

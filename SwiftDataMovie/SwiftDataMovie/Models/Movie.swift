736638db8ee523d2f96c317e00a9f1523a593481
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-20 08:35:44 -0400

    add model folder and movie model

--- SwiftDataMovie/SwiftDataMovie/Models/Movie.swift
+++ SwiftDataMovie/SwiftDataMovie/Models/Movie.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class Movie {
+    var title: String
+    var year: Int
+    
+    init(title: String, year: Int) {
+        self.title = title
+        self.year = year
+    }
+}

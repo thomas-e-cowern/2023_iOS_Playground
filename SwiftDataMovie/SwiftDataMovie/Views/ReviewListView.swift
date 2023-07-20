9019ad2ee6f0eea7df840aa9b09a2f7b1f7d31df
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-20 11:44:36 -0400

    update review list view with delete functionality

--- SwiftDataMovie/SwiftDataMovie/Views/ReviewListView.swift
+++ SwiftDataMovie/SwiftDataMovie/Views/ReviewListView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ReviewListView: View {
     
+    @Environment(\.modelContext) private var context
+    
     let reviews: [Review]
     
     var body: some View {
@@ -21,10 +23,25 @@ struct ReviewListView: View {
                     Text(review.body)
                 }
             }
+            .onDelete(perform: deleteReview)
+        }
+    }
+    
+    private func deleteReview(indexSet: IndexSet) {
+        indexSet.forEach { index in
+            let review = reviews[index]
+            context.delete(review)
+            
+            do {
+                try context.save()
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
 
 #Preview {
     ReviewListView(reviews: [])
+        .modelContainer(for: [Review.self, Movie.self])
 }

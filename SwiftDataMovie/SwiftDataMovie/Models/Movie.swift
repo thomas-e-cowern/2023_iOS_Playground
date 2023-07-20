4d3347a35853a5d757979566d30ea59f2e3b83cc
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-20 10:59:30 -0400

    add add review screen

--- SwiftDataMovie/SwiftDataMovie/Models/Movie.swift
+++ SwiftDataMovie/SwiftDataMovie/Models/Movie.swift
@@ -13,6 +13,8 @@ final class Movie {
     var title: String
     var year: Int
     
+    @Relationship(.cascade, inverse: \Review.movie) var reviews: [Review]? = []
+    
     init(title: String, year: Int) {
         self.title = title
         self.year = year
---
+++ SwiftDataMovie/SwiftDataMovie/Screens/AddReviewScreen.swift
@@ -0,0 +1,56 @@
+//
+//  AddReviewScreen.swift
+//  SwiftDataMovie
+//
+//  Created by Thomas Cowern on 7/20/23.
+//
+
+import SwiftUI
+import SwiftData
+
+struct AddReviewScreen: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var context
+    
+    let movie: Movie
+    
+    @State private var subject: String = ""
+    @State private var description: String = ""
+    
+    var body: some View {
+        Form {
+            TextField("Subject", text: $subject)
+            TextField("Description", text: $description)
+        }
+        .navigationTitle("Add Review")
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Close") {
+                    dismiss()
+                }
+                
+                Button("Save") {
+                    let review = Review(subject: subject, body: description, movie: movie)
+                    context.insert(review)
+                    
+                    do {
+                        try context.save()
+                        movie.reviews?.append(review)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+                .disabled(!isFormValid)
+            }
+        }
+    }
+    
+    private var isFormValid: Bool {
+        !subject.isEmptyOrWhiteSpaces && !description.isEmptyOrWhiteSpaces
+    }
+}
+
+//#Preview {
+//    AddReviewScreen()
+//}

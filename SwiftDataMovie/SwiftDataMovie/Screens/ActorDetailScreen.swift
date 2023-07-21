0f229c0d34f676f1162b16e2d3a398b800c13a76
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-21 09:55:17 -0400

    add actor detail screen and movie selection view

---
+++ SwiftDataMovie/SwiftDataMovie/Screens/ActorDetailScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ActorDetailScreen.swift
+//  SwiftDataMovie
+//
+//  Created by Thomas Cowern on 7/21/23.
+//
+
+import SwiftUI
+
+struct ActorDetailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActorDetailScreen()
+}
---
+++ SwiftDataMovie/SwiftDataMovie/Views/MovieSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieSelectionView.swift
+//  SwiftDataMovie
+//
+//  Created by Thomas Cowern on 7/21/23.
+//
+
+import SwiftUI
+
+struct MovieSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieSelectionView()
+}

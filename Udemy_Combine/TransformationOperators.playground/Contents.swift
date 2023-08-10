8246109d994c3497609c698e8954acbf829d03c6
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-10 14:00:20 -0400

    add collect and map learning to playground

--- Udemy_Combine/TransformationOperators.playground/Contents.swift
+++ Udemy_Combine/TransformationOperators.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
+import Combine
 
-var greeting = "Hello, playground"
+// Operators part 1
+
+// Collect
+["A", "B", "C", "D"].publisher.collect().sink {
+    print($0)
+}
+
+// Map
+let formatter = NumberFormatter()
+formatter.numberStyle = .spellOut
+
+[123, 45, 67, 987].publisher.map {
+    formatter.string(from: NSNumber(integerLiteral: $0))
+}.sink {
+    print($0 as Any)
+}

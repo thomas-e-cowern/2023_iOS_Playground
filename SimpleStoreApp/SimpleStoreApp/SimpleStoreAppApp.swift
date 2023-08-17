009f9ecf088d8182589ac9044070ef277170e6f1
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-17 10:55:11 -0400

    add simple store app

---
+++ SimpleStoreApp/SimpleStoreApp/SimpleStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleStoreAppApp.swift
+//  SimpleStoreApp
+//
+//  Created by Thomas Cowern on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

cb030de037de9ac73b283e8440f44c2c520589ab
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-17 12:57:15 -0400

    update list category screen with image

--- SimpleStoreApp/SimpleStoreApp/ContentView.swift
+++ SimpleStoreApp/SimpleStoreApp/ContentView.swift
@@ -9,19 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var results = [Category]()
-    @State private var viewModel = ListCategoryViewModel()
-    
     var body: some View {
-        List(results, id: \.id) { item in
-            VStack(alignment: .leading) {
-                Text(item.name)
-                    .font(.headline)
-                Text(item.creationAt)
-            }
-        }
-        .task {
-            results = await viewModel.loadData()
+        TabView {
+            ListCategoriesScreen()
+                .tabItem {
+                    Label("Categories", systemImage: "list.bullet")
+                }
         }
     }
 }
--- SimpleStoreApp/SimpleStoreApp/Screens/ListCategoriesScreen.swift
+++ SimpleStoreApp/SimpleStoreApp/Screens/ListCategoriesScreen.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct ListCategoriesScreen: View {
+    
+    @State private var results = [Category]()
+    @State private var viewModel = ListCategoryViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(results, id: \.id) { item in
+            VStack(alignment: .leading) {
+                HStack {
+                    AsyncImage(url:  URL(string: item.image), scale: 8)
+                        .padding(.trailing, 10)
+                    Text(item.name)
+                        .font(.headline)
+                }
+                
+            }
+        }
+        .task {
+            results = await viewModel.loadData()
+        }
     }
 }
 

de663fc03406f1355603770193a4e60e088be651
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-18 15:10:40 -0400

    update list product view with search fundtionality

--- SimpleStoreApp/SimpleStoreApp/Screens/ListProductsView.swift
+++ SimpleStoreApp/SimpleStoreApp/Screens/ListProductsView.swift
@@ -9,24 +9,35 @@ import SwiftUI
 
 struct ListProductsView: View {
     
+    @State private var searchText = ""
+    
     @State private var products = [Product]()
     @State private var viewModel = ListCategoryViewModel()
     
     var body: some View {
         NavigationView {
-            List(products, id: \.id) { product in
-                Text(product.title)
-//                NavigationLink {
-//                    CategoryScreen(category: category)
-//                } label: {
-//                    CategoryRowView(category: category)
-//                }
+            List(searchResults, id: \.id) { product in
+                NavigationLink {
+                    ProductsDetailScreen(product: product)
+                } label: {
+                    Text(product.title)
+                        .font(.headline)
+                }
             }
             .task {
                 products = await viewModel.loadProducts()
             }
         }
+        .searchable(text: $searchText)
     }
+    
+    var searchResults: [Product] {
+            if searchText.isEmpty {
+                return products
+            } else {
+                return products.filter { $0.title.contains(searchText) }
+            }
+        }
 }
 
 struct ListProductsView_Previews: PreviewProvider {

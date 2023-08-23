2048244a9065924e593059cd5d73ec3ea1fc9138
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-23 14:15:58 -0400

    update web service with fetch restrooms method

--- SwiftUIMap/SwiftUIMap/WebService.swift
+++ SwiftUIMap/SwiftUIMap/WebService.swift
@@ -20,8 +20,18 @@ struct WebService {
         case decodingError(Error)
     }
     
-    func fetchRestrooms(at url: URL) async throws -> [Restrooms] {
+    func fetchRestrooms(at url: URL) async throws -> [Restroom] {
         
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw WebServiceError.invalidResponse
+        }
+        
+        do {
+            return try JSONDecoder().decode([Restroom].self, from: data)
+        } catch {
+            throw WebServiceError.decodingError(error)
+        }
     }
-    
 }

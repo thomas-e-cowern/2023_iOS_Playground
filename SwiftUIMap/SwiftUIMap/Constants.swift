c7251e216b2169c6636363761448a629458e8f72
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-23 14:18:44 -0400

    add constants file

---
+++ SwiftUIMap/SwiftUIMap/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  SwiftUIMap
+//
+//  Created by Thomas Cowern on 8/23/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Urls {
+        static let restrooms = URL(string: "https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&lat=26.726880&lng=-80.116130")
+    }
+    
+}

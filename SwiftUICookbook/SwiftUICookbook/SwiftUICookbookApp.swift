e9e86be9ca473149013bdf067809f6320bbddd45
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-24 15:02:02 -0400

    add swiftui cookbook project

---
+++ SwiftUICookbook/SwiftUICookbook/SwiftUICookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICookbookApp.swift
+//  SwiftUICookbook
+//
+//  Created by Thomas Cowern on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

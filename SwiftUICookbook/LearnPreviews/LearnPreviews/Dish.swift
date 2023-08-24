eb0138db3626fdedf8256694657802026fed49c1
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-24 18:55:59 -0400

    add dish model

---
+++ SwiftUICookbook/LearnPreviews/LearnPreviews/Dish.swift
@@ -0,0 +1,14 @@
+//
+//  Dish.swift
+//  LearnPreviews
+//
+//  Created by Thomas Cowern on 8/24/23.
+//
+
+import Foundation
+
+struct Dish: Codable {
+    let title: String
+    let description: String
+    let photos: String
+}

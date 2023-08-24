1e57cb6c9ffb2f1f0f48631c046e5a3878a7e9ae
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-24 18:59:49 -0400

    update sample provider

--- SwiftUICookbook/LearnPreviews/LearnPreviews/SampleProvider.swift
+++ SwiftUICookbook/LearnPreviews/LearnPreviews/SampleProvider.swift
@@ -9,8 +9,16 @@ import Foundation
 
 class SampleProvider {
     
-    static func getDishSample() -> Dish {
+    static func getDishSample() -> Dish? {
+        guard let url = Bundle.main.url(forResource: "sample-dish", withExtension: "json"),
+              let data = try? Data(contentsOf: url) else {
+            fatalError("The resource was not found")
+        }
         
+        let dish = try? JSONDecoder().decode(Dish.self, from: data)
+        return dish
     }
+    
+    
     
 }

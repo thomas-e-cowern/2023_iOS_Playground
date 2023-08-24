3bb8fd5430ab59249a77e66bddfdb4e9ef51a383
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-24 18:54:54 -0400

    add sample dish json and sample provider class

---
+++ SwiftUICookbook/LearnPreviews/LearnPreviews/SampleProvider.swift
@@ -0,0 +1,16 @@
+//
+//  SampleProvider.swift
+//  LearnPreviews
+//
+//  Created by Thomas Cowern on 8/24/23.
+//
+
+import Foundation
+
+class SampleProvider {
+    
+    static func getDishSample() -> Dish {
+        
+    }
+    
+}

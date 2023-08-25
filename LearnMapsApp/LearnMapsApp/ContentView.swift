d4f73ce35ada401e1f2efb8b90071dd08c213747
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-25 14:10:10 -0400

    update map on content view to show annotations

--- LearnMapsApp/LearnMapsApp/ContentView.swift
+++ LearnMapsApp/LearnMapsApp/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 import MapKit
 
-struct Place {
+struct Place: Identifiable {
+    let id = UUID()
     let name: String
     let coordinate: CLLocationCoordinate2D
 }
 
 struct ContentView: View {
     
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 26.726880, longitude: -80.116130), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 26.726880, longitude: -80.116130), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
     
     let annotations = [
         Place(name: "1909", coordinate: CLLocationCoordinate2D(latitude: 26.712600, longitude: -80.052540)),
@@ -25,7 +26,9 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Map(coordinateRegion: $region)
+            Map(coordinateRegion: $region, annotationItems: annotations) {
+                MapPin(coordinate: $0.coordinate)
+            }
         }
         .padding()
     }

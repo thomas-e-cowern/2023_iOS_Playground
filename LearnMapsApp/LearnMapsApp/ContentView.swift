de0664dc84c7239c427d549358c26064ae1976b4
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-25 14:05:07 -0400

    add annotions array to content view

--- LearnMapsApp/LearnMapsApp/ContentView.swift
+++ LearnMapsApp/LearnMapsApp/ContentView.swift
@@ -17,6 +17,12 @@ struct ContentView: View {
     
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 26.726880, longitude: -80.116130), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
     
+    let annotations = [
+        Place(name: "1909", coordinate: CLLocationCoordinate2D(latitude: 26.712600, longitude: -80.052540)),
+        Place(name: "Duffy's", coordinate: CLLocationCoordinate2D(latitude: 26.713310, longitude: -80.100750)),
+        Place(name: "E.R. Bradley's", coordinate: CLLocationCoordinate2D(latitude: 26.712450, longitude: -80.049700))
+    ]
+    
     var body: some View {
         VStack {
             Map(coordinateRegion: $region)

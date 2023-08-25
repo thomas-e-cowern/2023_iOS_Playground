3488a8478438dd67d3258e10e62ac859f9cb8c4e
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-08-25 12:58:21 -0400

    add learn maps app

---
+++ LearnMapsApp/LearnMapsApp/LearnMapsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMapsAppApp.swift
+//  LearnMapsApp
+//
+//  Created by Thomas Cowern on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnMapsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

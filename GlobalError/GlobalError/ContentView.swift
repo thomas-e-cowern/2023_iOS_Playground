8b1a40b04d1af610bf5f9ed2e4c5627378829507
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-09-05 14:37:43 -0400

    add global error

---
+++ GlobalError/GlobalError/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GlobalError
+//
+//  Created by Thomas Cowern on 9/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GlobalError/GlobalError/GlobalErrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalErrorApp.swift
+//  GlobalError
+//
+//  Created by Thomas Cowern on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GlobalErrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

363919946a6a321cae643905aec66b3de5ada09f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-09-07 14:57:57 -0400

    add error wrapper variable, show error environment key and sheet to global error app

--- GlobalError/GlobalError/GlobalErrorApp.swift
+++ GlobalError/GlobalError/GlobalErrorApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct GlobalErrorApp: App {
+    
+    @State private var errorWrapper: ErrorWrapper?
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.showError) { error, guidance in
+                    errorWrapper = ErrorWrapper(error: error, guidance: guidance)
+                }
+                .sheet(item: $errorWrapper) { error in
+                    ErrorView(errorWrapper: error)
+                }
         }
     }
 }

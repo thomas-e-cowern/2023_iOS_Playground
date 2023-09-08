c6cc41a709b84a9a53eb12bf7733dffbcfdba7fd
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-09-08 15:23:20 -0400

    update content view with error view 2 and zstack

--- GlobalError/GlobalError/ContentView.swift
+++ GlobalError/GlobalError/ContentView.swift
@@ -7,6 +7,32 @@
 
 import SwiftUI
 
+class UsersViewModel: ObservableObject {
+    @Published var listUsers = [String]()
+    @Published var userError: UserError? = nil
+    
+    @MainActor
+    func loadUsers(withError: Bool) async {
+        if withError {
+            userError = UserError.failedLoading
+        } else {
+            userError = nil
+            listUsers = ["ninha", "Mike", "Pepijn"]
+        }
+    }
+}
+
+enum UserError: Error {
+    case failedLoading
+    
+    var description: String {
+        switch self {
+        case .failedLoading:
+            return "Sorry, we couldn't retrieve users. \n Try again later. ☹️"
+        }
+    }
+}
+
 struct ShowErrorEnvironmentKey: EnvironmentKey {
     static var defaultValue: (Error, String) -> Void = { _, _ in }
 }
@@ -36,8 +62,35 @@ struct ErrorView: View {
     }
 }
 
+struct ErrorView2: View {
+    let errorTitle: String
+    @ObservedObject var usersViewModel: UsersViewModel
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .foregroundColor(.red)
+            .overlay {
+                
+                VStack {
+                    Text(errorTitle)
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    
+                    Button("Reload Users") {
+                        Task {
+                            await usersViewModel.loadUsers(withError: false)
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                
+            }
+    }
+}
+
 struct ContentView: View {
     
+    @ObservedObject var usersViewModel = UsersViewModel()
     @Environment(\.showError) private var showError
     
     private enum SampleError: Error {
@@ -45,6 +98,19 @@ struct ContentView: View {
     }
     
     var body: some View {
+        ZStack {
+            List(usersViewModel.listUsers, id: \.self) { user in
+                Text(user)
+            }
+            
+            if let error = usersViewModel.userError { // << error handling here
+                ErrorView2(errorTitle: error.description, usersViewModel: usersViewModel)
+            }
+        }
+        .task {
+            try? await Task.sleep(for: .seconds(2)) // timer to fake the network request
+            await usersViewModel.loadUsers(withError: true) // calling the fake function with error
+        }
         VStack {
             Button("Throw Error") {
                 do {

1be51848f39871eb24360f7e18ebfb989a50155b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-07 15:15:33 -0400

    update content view with button and text

--- DynamicPropertyWrapper/DynamicPropertyWrapper/ContentView.swift
+++ DynamicPropertyWrapper/DynamicPropertyWrapper/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-@propertyWrapper struct Document {
+@propertyWrapper struct Document: DynamicProperty {
     @State private var value = ""
     private let url: URL
     
@@ -25,17 +25,31 @@ import SwiftUI
             }
         }
     }
+    
+    init(_ filename: String) {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        url = paths[0].appendingPathComponent(filename)
+        
+        let initialText = (try? String(contentsOf: url)) ?? ""
+        _value = State(wrappedValue: initialText)
+    }
 }
 
 struct ContentView: View {
+    
+    @Document("test.txt") var document
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text(document)
+                
+                Button("Change Document") {
+                    document = String(Int.random(in: 1...1000))
+                }
+            }
+            .navigationTitle("Simple Text")
         }
-        .padding()
     }
 }
 

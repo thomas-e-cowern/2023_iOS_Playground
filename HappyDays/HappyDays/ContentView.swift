1f1855df68c488f657573c2355aaa691e4c1cbd8
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-07 18:39:39 -0400

    happy days initial commit

---
+++ HappyDays/HappyDays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HappyDays
+//
+//  Created by Thomas Cowern on 10/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HappyDays/HappyDays/HappyDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyDaysApp.swift
+//  HappyDays
+//
+//  Created by Thomas Cowern on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct HappyDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

6fed8ae254e761d3f47f433c0d1a435686e1d796
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-07 14:36:04 -0400

    add messy json project

---
+++ MessyJSON/MessyJSON/MessyJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessyJSONApp.swift
+//  MessyJSON
+//
+//  Created by Thomas Cowern on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MessyJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

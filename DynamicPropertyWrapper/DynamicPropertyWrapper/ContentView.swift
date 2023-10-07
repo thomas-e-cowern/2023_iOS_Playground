00e13f25dfff0816b8fcb79e8f2a7d7df47b823f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-07 15:08:13 -0400

    add property wrapper struct

--- DynamicPropertyWrapper/DynamicPropertyWrapper/ContentView.swift
+++ DynamicPropertyWrapper/DynamicPropertyWrapper/ContentView.swift
@@ -7,6 +7,26 @@
 
 import SwiftUI
 
+@propertyWrapper struct Document {
+    @State private var value = ""
+    private let url: URL
+    
+    var wrappedValue: String {
+        get {
+            value
+        }
+        
+        nonmutating set {
+            do {
+                try newValue.write(to: url, atomically: true, encoding: .utf8)
+                value = newValue
+            } catch {
+                print("Failed to write output")
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {

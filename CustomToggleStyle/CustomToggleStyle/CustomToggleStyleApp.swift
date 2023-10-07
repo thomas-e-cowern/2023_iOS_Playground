3a61b406d9adaf2054549780bddad9d792dfd026
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-07 18:43:41 -0400

    add custom toggle style

---
+++ CustomToggleStyle/CustomToggleStyle/CustomToggleStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomToggleStyleApp.swift
+//  CustomToggleStyle
+//
+//  Created by Thomas Cowern on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomToggleStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

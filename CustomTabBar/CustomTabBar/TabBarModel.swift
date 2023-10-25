a7d4996093d5122e37dc581e3338d74a1d359824
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-25 16:15:41 -0400

    add tab bar model to project

---
+++ CustomTabBar/CustomTabBar/TabBarModel.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarModel.swift
+//  CustomTabBar
+//
+//  Created by Thomas Cowern on 10/25/23.
+//
+
+import SwiftUI
+
+struct TabBar: Identifiable {
+    var id = UUID()
+    var icon: String
+}

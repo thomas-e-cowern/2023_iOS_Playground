a693ed526020aa9d72dd5361901614fa5b1cfbc6
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-26 12:38:51 -0400

    add geometry reader to buttons in custom tab bar

--- CustomTabBar/CustomTabBar/CustomTabBar.swift
+++ CustomTabBar/CustomTabBar/CustomTabBar.swift
@@ -9,48 +9,57 @@ import SwiftUI
 
 struct CustomTabBar: View {
     var body: some View {
-        HStack {
+        HStack (alignment: .center) {
          
             Button {
                 // Action
             } label: {
-                VStack (alignment: .center, spacing: 4) {
-                    Image(systemName: "bubble.left")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
-                    Text("Chats")
-                        .font(Fonts.tabBar)
+                GeometryReader { geo in
+                    VStack (alignment: .center, spacing: 4) {
+                        Image(systemName: "bubble.left")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                        Text("Chats")
+                            .font(Fonts.tabBar)
+                    }
+                    .frame(width: geo.size.width, height: geo.size.height)
                 }
+                
             }
             .tint(Color("icons-secondary"))
             
             Button {
                 // New chat
             } label: {
-                VStack (alignment: .center, spacing: 4) {
-                    Image(systemName: "plus.circle.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 32, height: 32)
-                    Text("New Chat")
-                        .font(Fonts.tabBar)
+                GeometryReader { geo in
+                    VStack (alignment: .center, spacing: 4) {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 32, height: 32)
+                        Text("New Chat")
+                            .font(Fonts.tabBar)
+                    }
+                    .frame(width: geo.size.width, height: geo.size.height)
                 }
+                
             }
             .tint(Color("icons-primary"))
-
-            
             
             Button {
                 // Action
             } label: {
-                VStack (alignment: .center, spacing: 4) {
-                    Image(systemName: "person")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
-                    Text("Contacts")
-                        .font(Fonts.tabBar)
+                GeometryReader { geo in
+                    VStack (alignment: .center, spacing: 4) {
+                        Image(systemName: "person")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                        Text("Contacts")
+                            .font(Fonts.tabBar)
+                    }
+                    .frame(width: geo.size.width, height: geo.size.height)
                 }
             }
             .tint(Color("icons-secondary"))

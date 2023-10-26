26fb5c29c52b0c11f1c6b507364aa9c0f0aff9eb
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-26 12:31:08 -0400

    update custom tab bar with additional buttons

--- CustomTabBar/CustomTabBar/CustomTabBar.swift
+++ CustomTabBar/CustomTabBar/CustomTabBar.swift
@@ -23,6 +23,38 @@ struct CustomTabBar: View {
                         .font(Fonts.tabBar)
                 }
             }
+            .tint(Color("icons-secondary"))
+            
+            Button {
+                // New chat
+            } label: {
+                VStack (alignment: .center, spacing: 4) {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32, height: 32)
+                    Text("New Chat")
+                        .font(Fonts.tabBar)
+                }
+            }
+            .tint(Color("icons-primary"))
+
+            
+            
+            Button {
+                // Action
+            } label: {
+                VStack (alignment: .center, spacing: 4) {
+                    Image(systemName: "person")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                    Text("Contacts")
+                        .font(Fonts.tabBar)
+                }
+            }
+            .tint(Color("icons-secondary"))
+
 
         }
         .frame(height: 82)

6338de78f4952a0e56e3981c4587d8726aa4294a
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-28 07:35:33 -0400

    add custom tab bar 3

---
+++ CustomTabBar/CustomTabBar/CustomTabBar3/CustomTabBar3.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTabBar3.swift
+//  CustomTabBar
+//
+//  Created by Thomas Cowern on 10/28/23.
+//
+
+import SwiftUI
+
+enum Tab3: String, CaseIterable {
+    case house
+    case message
+    case person
+    case leaf
+    case gearshape
+}
+
+struct CustomTabBar3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBar3()
+}

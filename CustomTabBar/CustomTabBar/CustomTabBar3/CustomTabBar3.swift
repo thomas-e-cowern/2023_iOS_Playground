11c359836c20c7ec2d06cec96530d15b470be52f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-28 07:47:52 -0400

    update custom tab bar 3

--- CustomTabBar/CustomTabBar/CustomTabBar3/CustomTabBar3.swift
+++ CustomTabBar/CustomTabBar/CustomTabBar3/CustomTabBar3.swift
@@ -16,11 +16,37 @@ enum Tab3: String, CaseIterable {
 }
 
 struct CustomTabBar3: View {
+    
+    @Binding var selectedTab: Tab3
+    private var fillImage: String {
+        selectedTab.rawValue + ".fill"
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                ForEach(Tab3.allCases, id: \.rawValue) { tab in
+                    Spacer()
+                    Image(systemName: selectedTab == tab ? fillImage : tab.rawValue)
+                        .scaleEffect(selectedTab == tab  ? 1.25 : 1.0)
+                        .foregroundStyle(selectedTab == tab ? Color.red : Color.gray)
+                        .font(.system(size: 22))
+                        .onTapGesture {
+                            withAnimation(.easeIn(duration: 0.1)) {
+                                selectedTab = tab
+                            }
+                        }
+                    Spacer()
+                }
+            }
+            .frame(height: 60)
+            .background(.thinMaterial)
+            .clipShape(Rectangle())
+            .padding()
+        }
     }
 }
 
 #Preview {
-    CustomTabBar3()
+    CustomTabBar3(selectedTab: .constant(.house))
 }

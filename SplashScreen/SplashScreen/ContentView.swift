9aaecfb1e22b3ed3fbca2b8d8f46149f0ef9cdc5
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-28 09:03:47 -0400

    add splash screen project

---
+++ SplashScreen/SplashScreen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SplashScreen
+//
+//  Created by Thomas Cowern on 10/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplashScreen/SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by Thomas Cowern on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

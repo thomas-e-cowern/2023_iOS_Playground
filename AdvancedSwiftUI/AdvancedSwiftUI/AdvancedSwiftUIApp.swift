112489cd272c212b83a3838190ff36d5b376de43
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-28 12:29:08 -0400

    add advanced swiftui project

---
+++ AdvancedSwiftUI/AdvancedSwiftUI/AdvancedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedSwiftUIApp.swift
+//  AdvancedSwiftUI
+//
+//  Created by Thomas Cowern on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

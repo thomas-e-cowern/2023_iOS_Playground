622e7c21b02c37e0cc3ebc9b3941caf6f0be9da6
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-28 15:22:08 -0400

    update button style playground

--- AdvancedSwiftUI/AdvancedSwiftUI/CustomButtonStyle/ButtonStylePlayground.swift
+++ AdvancedSwiftUI/AdvancedSwiftUI/CustomButtonStyle/ButtonStylePlayground.swift
@@ -7,6 +7,28 @@
 
 import SwiftUI
 
+struct PressableButtonStyle: ButtonStyle {
+    
+    let scaledAmount: CGFloat
+    
+//    init(scaledAmount: CGFloat = 0.9) {
+//        self.scaledAmount = scaledAmount
+//    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .opacity(configuration.isPressed ? 0.8 : 1.0)
+//            .brightness(configuration.isPressed ? 0.05 : 0)
+            .scaleEffect(configuration.isPressed ? scaledAmount : 1.0)
+    }
+}
+
+extension View {
+    func withPressableStyle(scaledAmount: CGFloat = 0.9) -> some View {
+        self.buttonStyle(PressableButtonStyle(scaledAmount: scaledAmount))
+    }
+}
+
 struct ButtonStylePlayground: View {
     var body: some View {
         Button {
@@ -21,6 +43,7 @@ struct ButtonStylePlayground: View {
                 .clipShape(RoundedRectangle(cornerRadius: 10))
                 .shadow(color: Color.blue.opacity(0.3), radius: 10, x: 0.0, y: 10)
         }
+        .withPressableStyle()
         .padding(40)
 
     }

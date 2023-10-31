ea095e88a130d7bab1adc8d0f0c368b2ea63e0f1
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-31 14:25:51 -0400

    add matched geometry effect 2

---
+++ AdvancedSwiftUI/AdvancedSwiftUI/MatchedGeometryEffect/MatchedGeometryEffect2.swift
@@ -0,0 +1,38 @@
+//
+//  MatchedGeometryEffect2.swift
+//  AdvancedSwiftUI
+//
+//  Created by Thomas Cowern on 10/31/23.
+//
+
+import SwiftUI
+
+struct MatchedGeometryEffect2: View {
+    
+    let categories: [String] = ["Home", "Popular", "Saved"]
+    @State private var selected: String = ""
+    
+    var body: some View {
+        HStack {
+            ForEach(categories, id: \.self) { category in
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.red.opacity(0.5))
+                    Text(category)
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: 55)
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        selected = category
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MatchedGeometryEffect2()
+}

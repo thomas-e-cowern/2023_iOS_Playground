466d2c43ac7b33a2201480614902f7aede51456f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-31 14:34:02 -0400

    add generics playground

---
+++ AdvancedSwiftUI/AdvancedSwiftUI/Generics/GenericsPlayground.swift
@@ -0,0 +1,18 @@
+//
+//  GenericsPlayground.swift
+//  AdvancedSwiftUI
+//
+//  Created by Thomas Cowern on 10/31/23.
+//
+
+import SwiftUI
+
+struct GenericsPlayground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenericsPlayground()
+}

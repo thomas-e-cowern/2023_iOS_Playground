92f0d3075b56f6c045331ead99452b986703da39
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-31 14:28:38 -0400

    update matched geometry effect 2

--- AdvancedSwiftUI/AdvancedSwiftUI/MatchedGeometryEffect/MatchedGeometryEffect2.swift
+++ AdvancedSwiftUI/AdvancedSwiftUI/MatchedGeometryEffect/MatchedGeometryEffect2.swift
@@ -11,13 +11,17 @@ struct MatchedGeometryEffect2: View {
     
     let categories: [String] = ["Home", "Popular", "Saved"]
     @State private var selected: String = ""
+    @Namespace private var namespace
     
     var body: some View {
         HStack {
             ForEach(categories, id: \.self) { category in
                 ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color.red.opacity(0.5))
+                    if selected == category {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.red.opacity(0.5))
+                            .matchedGeometryEffect(id: "category_background", in: namespace)
+                    }
                     Text(category)
                 }
                 .frame(maxWidth: .infinity)

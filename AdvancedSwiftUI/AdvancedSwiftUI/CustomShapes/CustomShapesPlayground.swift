e1bfd072f5fdd41cc9a3bdaeffd36a55de1e4a37
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-01 09:58:20 -0400

    add custom shapes

---
+++ AdvancedSwiftUI/AdvancedSwiftUI/CustomShapes/CustomShapesPlayground.swift
@@ -0,0 +1,18 @@
+//
+//  CustomShapesPlayground.swift
+//  AdvancedSwiftUI
+//
+//  Created by Thomas Cowern on 11/1/23.
+//
+
+import SwiftUI
+
+struct CustomShapesPlayground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomShapesPlayground()
+}

7653577ada968e0e72096674eb43b735d40e25ed
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-01 11:14:25 -0400

    add diamond struct to content view

--- AdvancedSwiftUI/AdvancedSwiftUI/CustomShapes/CustomShapesPlayground.swift
+++ AdvancedSwiftUI/AdvancedSwiftUI/CustomShapes/CustomShapesPlayground.swift
@@ -18,16 +18,34 @@ struct Triangle: Shape {
     }
 }
 
+struct Diamond: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
 
 struct CustomShapesPlayground: View {
     var body: some View {
         ZStack {
             
-            Triangle()
-                .fill(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing))
-                .frame(width: 300, height: 300)
+            Diamond()
+//            Image("Naners")
+//                .resizable()
+//                .scaledToFit()
+//                .clipShape(Triangle())
+            
+//            Triangle()
+//                .fill(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing))
+//                .frame(width: 300, height: 300)
             
         }
+        .padding()
     }
 }
 

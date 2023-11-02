978e9c3d1368facea0ed21bfb52b1a581794a020
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-01 20:35:34 -0400

    add quad sample to content view

--- AdvancedSwiftUI/AdvancedSwiftUI/CustomShapes/ArcsQuadsPlayground.swift
+++ AdvancedSwiftUI/AdvancedSwiftUI/CustomShapes/ArcsQuadsPlayground.swift
@@ -47,10 +47,28 @@ struct ShapeWithArc: Shape {
             path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
             
             // Bottom
-            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+//            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))'
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY - 65),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 180),
+                clockwise: false)
             
             // Mid left
             path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+        }
+    }
+}
+
+struct QuadSample: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addQuadCurve(
+                to: CGPoint(x: rect.maxX, y: rect.maxY),
+                control: CGPoint(x: rect.midX, y: rect.midY)
+            )
         }
     }
 }

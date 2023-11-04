60cae3a5cd7288999ac6f43268876073bffc1e47
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-04 11:19:31 -0400

    update content view with ractangle with single corner animation

--- AdvancedSwiftUI/AdvancedSwiftUI/AnimatingCustomShapes/AnimatingCustomShapes.swift
+++ AdvancedSwiftUI/AdvancedSwiftUI/AnimatingCustomShapes/AnimatingCustomShapes.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct AnimatingCustomShapes: View {
+    
+    @State private var animate: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RectangleWithSingleCornerAnimation(cornerRadius: animate ? 60 : 0)
+                .frame(width: 230, height: 230)
+        }
+        .onAppear {
+            withAnimation(Animation.linear(duration: 2.0).repeatForever()) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+struct RectangleWithSingleCornerAnimation: Shape {
+    
+    var cornerRadius: CGFloat
+    
+    var animatableData: CGFloat {
+        get { cornerRadius }
+        set { cornerRadius = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - cornerRadius))
+            
+            path.addArc(center: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY - cornerRadius),
+                        radius: cornerRadius,
+                        startAngle: Angle(degrees: 0),
+                        endAngle: Angle(degrees: 360),
+                        clockwise: false)
+            
+            path.addLine(to: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
     }
 }
 

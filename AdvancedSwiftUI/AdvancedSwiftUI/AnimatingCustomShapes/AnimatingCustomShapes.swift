455b5bcfb79a12def7b6489a23c92baf8ab34450
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-04 11:01:49 -0400

    add animating custom shapes

---
+++ AdvancedSwiftUI/AdvancedSwiftUI/AnimatingCustomShapes/AnimatingCustomShapes.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatingCustomShapes.swift
+//  AdvancedSwiftUI
+//
+//  Created by Thomas Cowern on 11/4/23.
+//
+
+import SwiftUI
+
+struct AnimatingCustomShapes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatingCustomShapes()
+}

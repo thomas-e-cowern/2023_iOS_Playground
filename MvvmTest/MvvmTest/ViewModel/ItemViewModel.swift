461339f5fa7dddd6e89ebd6d83264ec74196db5e
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-28 22:06:56 -0500

    add item view model

---
+++ MvvmTest/MvvmTest/ViewModel/ItemViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  ItemViewModel.swift
+//  MvvmTest
+//
+//  Created by Thomas Cowern on 11/28/23.
+//
+
+import Foundation
+import SwiftUI
+
+class ItemViewModel: ObservableObject {
+    @Published var isTurnedOn: Bool = false
+    @Published var counter: Int = 0
+    @Published var itemList: [Item] = []
+    
+    func increment() {
+        counter += 1
+    }
+    
+    func addItem() {
+        let randomItems = ["Playstation", "Xbox", "Nintendo Wii", "Gameboy"]
+        let item = randomItems.randomElement()!
+        
+        let newItem = Item(name: item, description: "Item \(itemList.count + 1)")
+        
+        withAnimation {
+            itemList.insert(newItem, at: 0)
+        }
+    }
+    
+    func shuffle() {
+        itemList.shuffle()
+    }
+    
+    func removeFirst() {
+        itemList.removeFirst()
+    }
+    
+    func removeLast() {
+        itemList.removeLast()
+    }
+    
+    func clearList() {
+        itemList.removeAll()
+    }
+}

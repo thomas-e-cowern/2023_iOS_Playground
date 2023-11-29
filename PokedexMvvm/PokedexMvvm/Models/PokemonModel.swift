bc51d8ef7bba91d899ec4b65d70e5a96f17e9519
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-29 10:18:01 -0500

    add pokemon model

---
+++ PokedexMvvm/PokedexMvvm/Models/PokemonModel.swift
@@ -0,0 +1,28 @@
+//
+//  PokemonModel.swift
+//  PokedexMvvm
+//
+//  Created by Thomas Cowern on 11/29/23.
+//
+
+import Foundation
+
+struct PokemonPage: Codable {
+    let count: Int
+    let next: String
+    let results: [Pokemon]
+}
+
+struct Pokemon: Codable, Identifiable, Equatable {
+    let id = UUID()
+    let name: String
+    let url: String
+    
+    static var samplePokemon = Pokemon(name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/")
+}
+
+struct DetailPokemon: Codable {
+    let id: Int
+    let height: Int
+    let weight: Int
+}

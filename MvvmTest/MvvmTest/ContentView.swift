388ab13e5fca33592af32a3f9c2cd836dc219d3f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-28 22:16:03 -0500

    update content view with toggle, increment button, List and menu

--- MvvmTest/MvvmTest/ContentView.swift
+++ MvvmTest/MvvmTest/ContentView.swift
@@ -8,12 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var vm = ItemViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Toggle(isOn: $vm.isTurnedOn, label: {
+                Text("Toggle Me")
+            })
+            .padding()
+            
+            HStack {
+                Button("Increment") {
+                    vm.increment()
+                }
+                Text("\(vm.counter)")
+            }
+            
+            List(vm.itemList) { item in
+                HStack {
+                    Text(item.name)
+                    Spacer()
+                    Text(item.description)
+                }
+            }
+            .listStyle(.plain)
+            .background(.thinMaterial)
+            
+            Menu("Options") {
+                Button("Add Item") {
+                    vm.addItem()
+                }
+                Button("Remove First Item") {
+                    vm.removeFirst()
+                }
+                Button("Remove Last Item") {
+                    vm.removeLast()
+                }
+                Button("Clear List") {
+                    vm.clearList()
+                }
+            }
+            .padding()
         }
         .padding()
     }

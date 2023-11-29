0c07cb9e7ee42a6d8a343b5c23392a805123d4a7
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-29 15:45:18 -0500

    update content view

--- PokedexMvvm/PokedexMvvm/Views/ContentView.swift
+++ PokedexMvvm/PokedexMvvm/Views/ContentView.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var vm = ViewModel()
+    private let adaptiveColumns = [GridItem(.adaptive(minimum: 150))]
     
     var body: some View {
         NavigationStack {
-            
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 30) {
+                    ForEach(vm.filteredPokemon) { pokemon in
+                        NavigationLink(destination: PokemonDetailView(pokemon: pokemon)) {
+                            PokemonView(pokemon: pokemon)
+                        }
+                    }
+                }
+                .animation(.easeIn(duration: 0.3), value: vm.filteredPokemon.count)
+                .navigationTitle("Pokedex")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .searchable(text: $vm.searchText)
         }
         .environmentObject(vm)
     }

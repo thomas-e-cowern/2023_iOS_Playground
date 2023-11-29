77260b3b156fa9e5df7c433b883ee91784b1816b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-29 09:21:52 -0500

    pokedex mvvm initial commit

---
+++ PokedexMvvm/PokedexMvvm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokedexMvvm
+//
+//  Created by Thomas Cowern on 11/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokedexMvvm/PokedexMvvm/PokedexMvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexMvvmApp.swift
+//  PokedexMvvm
+//
+//  Created by Thomas Cowern on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexMvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

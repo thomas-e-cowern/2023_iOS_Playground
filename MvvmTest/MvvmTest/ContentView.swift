c81a1c65b377b21cf0ce7405d66a7d8120da2a0c
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-28 21:53:29 -0500

    mvvm test initial commit

---
+++ MvvmTest/MvvmTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MvvmTest
+//
+//  Created by Thomas Cowern on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MvvmTest/MvvmTest/MvvmTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MvvmTestApp.swift
+//  MvvmTest
+//
+//  Created by Thomas Cowern on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MvvmTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

aef279c39d8a6fd839a059ce930b9212baab7e08
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-11-29 10:24:56 -0500

    add pokemon manager

---
+++ PokedexMvvm/PokedexMvvm/Managers/PokemonManager.swift
@@ -0,0 +1,26 @@
+//
+//  PokemonManager.swift
+//  PokedexMvvm
+//
+//  Created by Thomas Cowern on 11/29/23.
+//
+
+import Foundation
+
+class PokemonManager {
+    func getPokemon() -> [Pokemon] {
+        let data: PokemonPage = Bundle.main.decode(file: "pokemon.json")
+        let pokemon: [Pokemon] = data.results
+        
+        return pokemon
+    }
+    
+    func getDatailedPokemon(id: Int, _ completion:@escaping (DetailPokemon) -> ()) {
+        Bundle.main.fetchData(url: "https://pokeapi.co/api/v2/pokemon/\(id)/", model: DetailPokemon.self) { data in
+            completion(data)
+        } failure: { error in
+            print(error)
+        }
+
+    }
+}

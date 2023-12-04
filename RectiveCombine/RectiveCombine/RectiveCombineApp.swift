b532fb292d554dc0d0a2c784364cebdbb871262b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-12-04 15:51:07 -0500

    update app file with init to accept notifications when phone rotates

--- RectiveCombine/RectiveCombine/RectiveCombineApp.swift
+++ RectiveCombine/RectiveCombine/RectiveCombineApp.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import Combine
 
 @main
 struct RectiveCombineApp: App {
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    init() {
+        NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)
+            .sink { _ in
+                let currentOrientation = UIDevice.current.orientation
+                print(currentOrientation)
+            }.store(in: &cancellables)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()

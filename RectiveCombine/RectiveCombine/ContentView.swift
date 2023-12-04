52b08ff77e4d190f4c207440cd7a05d6069636d9
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-12-04 15:40:30 -0500

    ReactiveCombine initial commit

---
+++ RectiveCombine/RectiveCombine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RectiveCombine
+//
+//  Created by Thomas Cowern on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RectiveCombine/RectiveCombine/RectiveCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectiveCombineApp.swift
+//  RectiveCombine
+//
+//  Created by Thomas Cowern on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct RectiveCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

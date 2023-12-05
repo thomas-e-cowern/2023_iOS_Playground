60f9030eb7873e19b1b44fee72016ac3795e754c
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-12-05 15:40:26 -0500

    update view builder playground with header view generic

--- AdvancedSwiftUI/AdvancedSwiftUI/Viewbuilder/ViewbuilderPlayground.swift
+++ AdvancedSwiftUI/AdvancedSwiftUI/Viewbuilder/ViewbuilderPlayground.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct HeaderViewRegular: View {
+    
+    let title: String
+    let description: String?
+    
     var body: some View {
         VStack (alignment: .leading) {
             Text("Title")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
-            Text("Description")
-                .font(.callout)
+            
+            if let description = description {
+                Text(description)
+                    .font(.callout)
+            }
+            
+            RoundedRectangle(cornerRadius: 5)
+                .frame(height: 2)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+    }
+}
+
+struct HeaderViewGeneric<Content:View>: View {
+    
+    let title: String
+    let content: Content
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            content
             
             RoundedRectangle(cornerRadius: 5)
                 .frame(height: 2)
@@ -28,7 +56,16 @@ struct ViewbuilderPlayground: View {
     var body: some View {
         VStack {
             
-            HeaderViewRegular()
+            HeaderViewRegular(title: "Test title", description: nil)
+            HeaderViewRegular(title: "Another title", description: "This is a descrtiption")
+            HeaderViewGeneric(title: "Generic View", content: Text("Hello"))
+            HeaderViewGeneric(title: "Image", content: Image(systemName: "heart.fill"))
+            HeaderViewGeneric(title: "Three", content: HStack {
+                Text("Hearts!")
+                Image(systemName: "heart.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(Color.red)
+            })
             
             Spacer()
         }

d8fa6ddbef82bff940a09ca50dd9a8aef1624e99
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-12-05 15:23:00 -0500

    add viewbuilder playground

---
+++ AdvancedSwiftUI/AdvancedSwiftUI/Viewbuilder/ViewbuilderPlayground.swift
@@ -0,0 +1,24 @@
+//
+//  ViewbuilderPlayground.swift
+//  AdvancedSwiftUI
+//
+//  Created by Thomas Cowern on 12/5/23.
+//
+
+import SwiftUI
+
+struct ViewbuilderPlayground: View {
+    var body: some View {
+        VStack {
+            Text("Title")
+            Text("Description")
+            
+            Spacer()
+        }
+        
+    }
+}
+
+#Preview {
+    ViewbuilderPlayground()
+}

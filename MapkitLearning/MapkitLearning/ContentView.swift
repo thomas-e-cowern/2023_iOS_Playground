3994a2aa1552a2fc374b1a13d144717e3f98965b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-12-24 17:33:16 -0500

    mapkitlearning initial commit

---
+++ MapkitLearning/MapkitLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapkitLearning
+//
+//  Created by Thomas Cowern on 12/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapkitLearning/MapkitLearning/MapkitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapkitLearningApp.swift
+//  MapkitLearning
+//
+//  Created by Thomas Cowern on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MapkitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

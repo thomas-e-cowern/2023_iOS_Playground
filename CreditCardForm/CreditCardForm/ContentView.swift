f1e2ffec693f64d371cda48a2b99d0175fd7dc82
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-05 15:16:10 -0500

    credit card form initial commit

---
+++ CreditCardForm/CreditCardForm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CreditCardForm
+//
+//  Created by Thomas Cowern on 1/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CreditCardForm/CreditCardForm/CreditCardFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardFormApp.swift
+//  CreditCardForm
+//
+//  Created by Thomas Cowern on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

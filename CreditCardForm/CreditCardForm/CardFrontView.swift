fb61e9b9b3e809e738d711fe144027565794fa0b
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-05 15:28:15 -0500

    update content view with card symbols and info spaces

--- CreditCardForm/CreditCardForm/CardFrontView.swift
+++ CreditCardForm/CreditCardForm/CardFrontView.swift
@@ -10,7 +10,41 @@ import SwiftUI
 struct CardFrontView: View {
     var body: some View {
         VStack {
+            HStack {
+                Image(systemName: "checkmark.circle.fill")
+                    .padding()
+                Spacer()
+                Text("VISA")
+                    .padding(.trailing, 10)
+                    .font(.system(size: 18, weight: .bold, design: .monospaced))
+                    .italic()
+            }
             
+            Text("4242-4242-4242-4242")
+                .font(.system(size: 26, weight: .bold, design: .rounded))
+                .padding()
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Card Holder Name")
+                        .opacity(0.5)
+                        .font(.system(size: 14))
+                    Text("Thomas E Cowern")
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .leading) {
+                    Text("Expires")
+                        .opacity(0.5)
+                        .font(.system(size: 14))
+                    Text("06/11")
+                }
+                
+            }
+            .padding()
+            
+            Spacer()
         }
         .foregroundStyle(.white)
         .frame(width: 350, height: 250)
